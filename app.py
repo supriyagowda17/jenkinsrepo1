3112193016e9f7c75b1e80a11de5fc7dd463930e
supriyagowda17 <119667906+supriyagowda17@users.noreply.github.com>
2026-04-09 16:18:18 +0530

    c3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-        return "<h1>Python WebApp Demo-latest</h1>"
+        return "<h1>Python WebApp Demo-using jenkins CI feature</h1>"
 
 if __name__ == "__main__":
         app.run(host="0.0.0.0" , port=8000)
